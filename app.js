7706bd7c1872dbc3ff48ec783ceeaf405d5fc415
Jose Tomás Hervás <josetomashervas@gmail.com>
2021-07-24 12:02:46 +0200

    agregado infinite scroll

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
+import {API} from './env.js';
+let listaHtml = '';
+let limit = 24
+let offset = 0;
+let contenedor = document.querySelector('#row');
+
 const marvel = () => {
-    const urlAPI = `http://gateway.marvel.com/v1/public/characters?limit=24&ts=1&apikey=5c72b03216bfdf7ec53f405d8abb2718&hash=979491c3699536e3c55dce60b46b0c9a`;
-    const contenedor = document.querySelector('#row');
-    let listaHtml = '';
+    const urlAPI = `http://gateway.marvel.com/v1/public/characters?limit=${limit}&offset=${offset}&ts=1&apikey=${API.API_KEY}&hash=${API.HASH}`;
+
     fetch(urlAPI)
         .then(res => res.json())
         .then(({ data }) => {
-            data.results.map(result => {
-                console.log(result);
+            data.results.map(hero => {
+                
                 listaHtml += `<div class="cajaImagen">
-                                <a href="${result.urls[0].url}">
-                                    <img src="${result.thumbnail.path}.${result.thumbnail.extension}" alt="${result.name}">
+                                <a href="${hero.urls[0].url}">
+                                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}">
 
                                     <div class="cajaNombre">
-                                        <p class="nombreHeroe">${result.name}</p>
+                                        <p class="nombreHeroe">${hero.name}</p>
                                     </div>
                                 </a>
                             </div>`
             })
             contenedor.innerHTML = listaHtml;
+            //console.log(contenedor.lastElementChild);
+            console.log(contenedor.childElementCount)
+            setObserver();
         })
+}
+
+const callback = (entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            console.log(entry)
+            offset = offset + 24;
+            console.log(offset)
+            marvel();
+        }})
+}
+
+
+const setObserver = ()=>{
+    const options = {
+        threshold:0.1
+    }
+
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(contenedor.lastElementChild);
 }
 
 marvel();
