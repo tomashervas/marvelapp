3d10a81042ea68e0f4562b0468c888e6752d9145
Jose Tomás Hervás <josetomashervas@gmail.com>
2021-07-24 12:02:46 +0200

    arreglado infinit scroll

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ let limit = 24;
 let offset = 0;
 let contenedor = document.querySelector('#row');
 const input = document.querySelector('input');
+const inp = document.getElementById('inp');
+const nav = document.getElementById('nav');
 let query = '';
 const sinImagen = '_not_';
 const noImg = '4c002e0305708';
+let contSinImg = 0;
 
-function ocultar(){
-    input.classList.toggle('oculta');
+function ocultar(el){
+    el.classList.toggle('oculta');
 }
 
 const updateValue = (e) => {
@@ -17,12 +20,14 @@ const updateValue = (e) => {
     console.log(e.target.value);
     listaHtml = '';
     offset=0;
+    contSinImg = 0;
     query= (busqueda) ?  `nameStartsWith=${busqueda}&` : '';
     marvel();
 }
 
 input.addEventListener('change', updateValue);
-const buscarBtn = document.getElementById('buscarBtn').addEventListener('click', ocultar);
+const buscarBtn = document.getElementById('buscarBtn').addEventListener('click', ()=>ocultar(inp));
+const menuBtn = document.getElementById('menuBtn').addEventListener('click', ()=>ocultar(nav));
      
 
 const marvel = () => {
@@ -33,7 +38,7 @@ const marvel = () => {
         .then(({ data }) => {
             console.log(data)
             data.results.map(hero => {
-                if (hero.thumbnail.path.includes(sinImagen) || hero.thumbnail.path.includes(noImg)) return;
+                if (hero.thumbnail.path.includes(sinImagen) || hero.thumbnail.path.includes(noImg)) return contSinImg++;
 
                 listaHtml += `<div class="cajaImagen">
                                 <a href="${hero.urls[0].url}">
@@ -47,9 +52,11 @@ const marvel = () => {
             })
             contenedor.innerHTML = listaHtml;
             //console.log(contenedor.lastElementChild);
+            console.log('total ' + data.total)
             console.log('Elementos en lista: ' + contenedor.childElementCount)
+            console.log('cont SIN ' + contSinImg)
 
-            if(data.total > contenedor.childElementCount){
+            if(data.total > (contenedor.childElementCount + contSinImg)){
                 setObserver();
             }
         })
